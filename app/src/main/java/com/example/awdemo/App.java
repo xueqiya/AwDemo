843b10445368b6baf9e437dc5779a4a1f9e91ae0
XueQi <xueqi@zjmedia.net>
2020-12-03 13:58:47 +0800

    feat: init

---
+++ app/src/main/java/com/example/awdemo/App.java
@@ -0,0 +1,6 @@
+package com.example.awdemo;
+
+import org.chromium.android_webview.shell.AwShellApplication;
+
+public class App extends AwShellApplication {
+}
---
+++ app/src/main/java/com/example/awdemo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.awdemo
+
+import org.chromium.android_webview.shell.AwShellActivity
+
+class MainActivity : AwShellActivity() {
+}
